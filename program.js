36edd43142f74d0ed1f52172881592f4fba40fb9
beekley <brettbeekley@gmail.com>
2017-03-31 16:14:53 -0700

    Now pulls spotify IDs!

--- program.js
+++ program.js
@@ -13,47 +13,55 @@ var artists = [];
 event.then(function(value) {
     
     value.results.event.performance.forEach(function(artist, i) {
-        console.log(artist.displayName);
-        artists.push({displayName: artist.displayName,
-                     billing: i});
+        //console.log(artist.displayName);
+        
+        // add spotify ID (sid) to the artist's object in artists[]
+        getSpotifyArtistId(artist.displayName, function(sid) {
+            
+            // add the artist to the artists[] list
+            artists[i] = {displayName: artist.displayName,
+                                        billing: i,
+                                        sid: sid};
+            
+            console.log(artists[i]);
+        });
+        
     });
     
-    // search for top artist in spotify
-    /*var options = {
-        'path':'/v1/search?q=Muse&type=track,artist&market=US HTTP/1.1',
-        'Host': 'api.spotify.com',
-        'Accept-Encoding': 'gzip, deflate, compress',
-        'Accept': 'application/json'
-    };*/
-    
+    //console.log(artists);
 });
 
-// testing http
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write('Billing: Artist');
-    artists.forEach(function(artist) {
-        response.write(artist.billing + ': ' + artist.displayName + '\n');
-    })
-    response.end();
-}).listen(8888);
-
-// testing spotify endpoint
-function getArtist(artistName) {
+// takes in an arist's displayname and callback function
+// callback has one parameter, the spotify arist ID (sid)
+function getSpotifyArtistId(artistName, callback) {
+    
     var options = {
         host: 'api.spotify.com',
-        path: '/v1/search?q=' + artistName + '&type=artist&market=US&limit=1',
+        path: '/v1/search?q=' + encodeURIComponent(artistName) + '&type=artist&market=US&limit=1',
         port: 443,
         method: 'GET'
     }
-
+    
+    //console.log(options);
+    
     var request = https.request(options, function(response) {
-        console.log('STATUS: ' + response.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(response.headers));
+        //console.log('STATUS: ' + response.statusCode);
+        //console.log('HEADERS: ' + JSON.stringify(response.headers));
+        
         response.setEncoding('utf8');
         response.on('data', function (chunk) {
-            //console.log('BODY: ' + chunk);
-            console.log(JSON.parse(chunk).artists.items[0].id);
+            
+            if(JSON.parse(chunk).artists.total > 0) {
+                var sid = JSON.parse(chunk).artists.items[0].id
+
+                //console.log(sid);
+                callback(sid);
+            } else {
+                console.log('No results found at ' + options.path);
+            }
+            
+            
+            
         });
     });
 
@@ -64,3 +72,12 @@ function getArtist(artistName) {
     request.end();
 }
 
+// testing http
+/*http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type":"text/plain"});
+    response.write('Billing: Artist');
+    for (var artist in artists) {
+        response.write(artist.billing + ': ' + artist.displayName + ': ' + artist.sid + '\n');
+    }
+    response.end();
+}).listen(8888);*/
