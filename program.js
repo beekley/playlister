87da94aef6f8a2c1ffb4164ac8a94f1269f2082d
beekley <brettbeekley@gmail.com>
2017-03-30 17:22:18 -0700

    Added first spotify hooks

--- program.js
+++ program.js
@@ -1,5 +1,6 @@
 var songkick_wrapper = require('songkick-wrapper');
 var http = require('http');
+var https = require('https');
 var keys = require('./apiKeys.js');
 
 var songkick = songkick_wrapper.create(keys.songkick);
@@ -8,19 +9,13 @@ var event = songkick.getEventDetails(process.argv[2]);
 
 var artists = [];
 
-/*http.get('http://api.spotify.com/v1/search?q=Muse&type=track,artist&market=US', function(response) {
-    response.setEncoding('utf8');
-    response.on('data', function (body) {
-        console.log(body);
-    });
-});*/
-
 // get list of artists for the inputted event ID
 event.then(function(value) {
     
-    value.results.event.performance.forEach(function(artist) {
+    value.results.event.performance.forEach(function(artist, i) {
         console.log(artist.displayName);
-        artists.push(artist.displayName);
+        artists.push({displayName: artist.displayName,
+                     billing: i});
     });
     
     // search for top artist in spotify
@@ -36,8 +31,36 @@ event.then(function(value) {
 // testing http
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type":"text/plain"});
+    response.write('Billing: Artist');
     artists.forEach(function(artist) {
-        response.write(artist + '\n');
+        response.write(artist.billing + ': ' + artist.displayName + '\n');
     })
     response.end();
 }).listen(8888);
+
+// testing spotify endpoint
+function getArtist(artistName) {
+    var options = {
+        host: 'api.spotify.com',
+        path: '/v1/search?q=' + artistName + '&type=artist&market=US&limit=1',
+        port: 443,
+        method: 'GET'
+    }
+
+    var request = https.request(options, function(response) {
+        console.log('STATUS: ' + response.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(response.headers));
+        response.setEncoding('utf8');
+        response.on('data', function (chunk) {
+            //console.log('BODY: ' + chunk);
+            console.log(JSON.parse(chunk).artists.items[0].id);
+        });
+    });
+
+    request.on('error', function(error) {
+        console.log('ERROR: ' + error);
+    })
+
+    request.end();
+}
+
